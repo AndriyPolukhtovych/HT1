a33e1ca32e683accb24f5e4fdb7a60334f32c4cb
Andriy <andriy_polukhtovych@epam.com>
2023-11-03 12:24:24 +0200

    fix

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,34 +1,29 @@
-
-const { expect } = require('@wdio/globals')
+const { expect, browser, $ } = require('@wdio/globals')
+const path = require('path');
+const fs = require('fs');
 
 
 describe('EPAM', () => {
     beforeEach(async() =>{
         await browser.url("https://www.epam.com/");
         await browser.maximizeWindow();
-        
-    });
+     });
 
     it('Check the title is correct', async () => {
-        
         const pageTitle=await browser.getTitle();
         expect(pageTitle).toEqual("EPAM | Software Engineering & Product Development Services")
-        
-    })
+     })
 
     it("Check the ability to switch Light / Dark mode", async ()=>{
         const toggle= await $$(`.theme-switcher`)[1];
-        //await browser.moveToElement(toggle);
         const body= await $$("BODY")[0];
         await toggle.waitForDisplayed();
-                
         await toggle.click();
         await browser.pause(2000);
         color= await body.getAttribute("class");
         expect(color).toBe("fonts-loaded light-mode")
-        
+     })
 
-    })
     it("Check that allow to change language to UA", async()=>{
         const langButton= await $(".location-selector__button");
         await langButton.click();
@@ -38,37 +33,25 @@ describe('EPAM', () => {
         await list.waitForDisplayed();
         const ukr= await list.$$("li")[5].$('a');
         await ukr.waitForClickable();
-        
         await ukr.click();
-        await browser.pause(3000);
-        //const vaca= await browser.getUrl()
-        
-        
+        await browser.pause(3000);    
         expect( await browser.getUrl()).toBe("https://careers.epam.ua/");
-
-
-
-
     })
 
     it("Check the policies list", async()=>{
         await browser.url("https://www.epam.com/");
-
         const policies = await $(".policies-links-wrapper");
         await policies.scrollIntoView({ block: "center", behavior: "smooth" });
-        
         await policies.waitForDisplayed();
         await browser.pause(2000);
         const policiesLeft= await $$(".ul.policies-left")[0];
         const policiesRight=await $$(".ul.policies-right")[0];
-        // await policiesLeft.scrollIntoView();
         expect(await policiesLeft.$$("li")[0].$('a').getText()).toBe("INVESTORS");
         expect(await  policiesLeft.$$("li")[1].$('a').getText()).toBe("OPEN SOURCE");
         expect(await  policiesLeft.$$("li")[2].$('a').getText()).toBe("PRIVACY POLICY");
         expect(await  policiesRight.$$("li")[0].$('a').getText()).toBe("COOKIE POLICY");
         expect(await  policiesRight.$$("li")[1].$('a').getText()).toBe("APPLICANT PRIVACY NOTICE");
         expect(await  policiesRight.$$("li")[2].$('a').getText()).toBe("WEB ACCESSIBILITY");
-
     })
 
     it("Check that allow to switch location list by region", async()=>{
@@ -83,7 +66,6 @@ describe('EPAM', () => {
         expect(emea).toBeDisplayed();
         expect(apac).toBeDisplayed();
         await emea.click();
-        await loc.scrollIntoView({ block: "center", behavior: "smooth" });
         await browser.pause(2000);
         const coun= await $(`[data-country-title="Armenia"]`);
         expect(coun).toBeDisplayed();
@@ -93,7 +75,6 @@ describe('EPAM', () => {
     })
 
     it("Check the search function", async()=>{
-
         const sb= await $(`button[class="header-search__button header__icon"]`);
         await sb.click();
         const sf= await $("#new_form_search");
@@ -103,9 +84,6 @@ describe('EPAM', () => {
         await but.click();
         await browser.pause(2000);
         expect(await browser.getUrl()).toBe("https://www.epam.com/search?q=AI");
-
-
-
     })
 
     it("Chack form's fields validation", async()=>{
@@ -115,33 +93,25 @@ describe('EPAM', () => {
         let req= await $$(`.validation-field`);
         expect( req.length).toBe(0);
         const button= await $(`.button-ui`);
-        // await button.scrollIntoView();
         await button.waitForDisplayed()
-        // await browser.pause(2000);
         await button.click();
         await browser.pause(2000);
         req= await $$(`.validation-field`);
         expect(req.length).toBe(6);
-
-        
-
-
-
     })
+
     it("Check tha the Company logo on the header lead to the main page", async()=>{
         await browser.url("https://www.epam.com/about");
         const logo= await $(`a[class="header__logo-container desktop-logo"]`);
         await logo.waitForDisplayed();
         await logo.click();
         await browser.pause(2000);
-
         expect(await browser.getUrl()).toBe("https://www.epam.com/")
     })
 
     it("Check that allows to download report", async()=>{
         await browser.url("https://www.epam.com/about");
         const button= await $(`a[class="button-ui-23 btn-focusable"]`);
-        //await button.scrollIntoView({ block: "center", behavior: "smooth" });
         await  button.waitForDisplayed();
         await button.click();
         await browser.pause(2000);
@@ -150,19 +120,11 @@ describe('EPAM', () => {
         const full= downUrl.pathname;
         const split=full.split(`/`);
         const fileName= split.splice(-1)[0];
-        //const filepath= path.join(global.downloadDir, fileName);
-        const ext =fileName.split(".").splice(1)[0];
-        const name=fileName.split(".").splice(0,1)[0];
-
+        const filepath= path.join(global.downloadDir, fileName);
+        const { name, ext } = path.parse(filepath);
         await browser.pause(5000);
-        
         expect(name).toBe("EPAM_Corporate_Overview_Q3_october")
-        expect(ext).toBe("pdf")
-
-
-
-
+        expect(ext.slice(1)).toBe("pdf")
     })
 
 })
-
--- wdio.conf.js
+++ wdio.conf.js
@@ -74,16 +74,12 @@ exports.config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome',
-        'goog:chromeOptions': {
-            prefs: {
-                'download.default_directory': downloadDir
-                 }}
+        browserName: 'chrome'
     }, 
     // {
     //     browserName: 'firefox'
     // }
-    ],
+],
 
     //
     // ===================
@@ -177,11 +173,8 @@ exports.config = {
      * @param {object} config wdio configuration object
      * @param {Array.<Object>} capabilities list of capabilities details
      */
-    onPrepare: function (config, capabilities) {
-        if(!fs.existsSync(downloadDir)){
-            fs.mkdirSync(downloadDir)
-        }
-    },
+    // onPrepare: function (config, capabilities) {
+    // },
     /**
      * Gets executed before a worker process is spawned and can be used to initialise specific service
      * for that worker as well as modify runtime environments in an async fashion.
@@ -305,9 +298,8 @@ exports.config = {
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {<Object>} results object containing test results
      */
-    onComplete: function(exitCode, config, capabilities, results) {
-        rmdir(downloadDir)
-    },
+    // onComplete: function(exitCode, config, capabilities, results) {
+    // },
     /**
     * Gets executed when a refresh happens.
     * @param {string} oldSessionId session ID of the old session
